6945a166b34912ac699ef59808c2380032541dad
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-24 10:38:55 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,16 +7,16 @@ def parseLogs():
     Reads stdin line by line and computes metrics
     """
     stdin = __import__('sys').stdin
-    lineNumber = 0
-    fileSize = 0
+    line = 0
+    size = 0
     statusCodes = {}
     codes = ('200', '301', '400', '401', '403', '404', '405', '500')
     try:
         for line in stdin:
-            lineNumber += 1
+            line += 1
             line = line.split()
             try:
-                fileSize += int(line[-1])
+                size += int(line[-1])
                 if line[-2] in codes:
                     try:
                         statusCodes[line[-2]] += 1
@@ -24,20 +24,20 @@ def parseLogs():
                         statusCodes[line[-2]] = 1
             except (IndexError, ValueError):
                 pass
-            if lineNumber == 10:
-                report(fileSize, statusCodes)
-                lineNumber = 0
-        report(fileSize, statusCodes)
+            if line == 10:
+                report(size, statusCodes)
+                line = 0
+        report(size, statusCodes)
     except KeyboardInterrupt as e:
-        report(fileSize, statusCodes)
+        report(size, statusCodes)
         raise
 
 
-def report(fileSize, statusCodes):
+def report(size, statusCodes):
     """
     Prints the log size and status codes
     """
-    print("File size: {}".format(fileSize))
+    print("File size: {}".format(size))
     for key, value in sorted(statusCodes.items()):
         print("{}: {}".format(key, value))
 
