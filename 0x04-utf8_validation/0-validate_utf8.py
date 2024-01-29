ac201be758e7cfd564a75712dfd04d21b227c63c
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-29 08:40:47 +0300

    utf-validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """
+    determines if a given data set represents a valid UTF-8 encoding.
+    """
+    bytes = 0
+    for uni in data:
+        byte = format(uni, '#010b')[-8:]
+        if bytes == 0:
+            for bit in byte:
+                if bit == '0':
+                    break
+                bytes += 1
+            if bytes == 0:
+                continue
+            if bytes == 1 or bytes > 4:
+                return False
+        else:
+            if not (byte[0] == '1' and byte[1] == '0'):
+                return False
+        bytes -= 1
+    return bytes == 0
