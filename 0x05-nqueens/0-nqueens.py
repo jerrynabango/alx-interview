3ee8e583baffbcd449d09705a5e72eda8a21bcb3
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-10 00:13:15 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,105 +1,63 @@
 #!/usr/bin/python3
-"""N Queens Problem Solver"""
+"""solves the N queens problem"""
+import sys
+import re
 
-from sys import argv
 
-def check_row(board, index, board_len):
-    """Check for queen in row"""
-    for r in range(board_len):
-        if board[index][r]:
-            return False
-    return True
+def n_q(t_arr, arr, col, i, n):
+    """
+       n_q - Find all posibles solution for N-queen problem and return it
+             in a list
+       @t_arr: temporaly list to store the all points of a posible solution
+       @arr: store all the solution
+       @col: save a colum use for a queen
+       @i: the row of the chess table
+       @n: Number of queens
+    """
+    if (i > n):
+        arr.append(t_arr[:])
+        return arr
 
-def check_r_angle(board, row, col, board_len):
-    """Check left angle for queen"""
-    c = col
-    for r in range(row, -1, -1):
-        if c >= board_len:
-            break
-        if board[r][c]:
-            return False
-        c += 1
-    c = col
-    for r in range(row, board_len):
-        if c < 0:
-            break
-        if board[r][c]:
-            return False
-        c -= 1
-    return True
+    for j in range(n + 1):
+        if i == 0 or (not ([i - 1, j - 1] in t_arr) and
+                      not ([i - 1, j + 1] in t_arr) and
+                      j not in col):
+            if i > 1:
+                dia = 0
+                for k in range(2, i + 1):
+                    if ([i - k, j - k] in t_arr) or ([i - k, j + k] in t_arr):
+                        dia = 1
+                        break
+                if dia:
+                    continue
+            t_arr.append([i, j])
+            col.append(j)
+            n_q(t_arr, arr, col, i + 1, n)
+            col.pop()
+            t_arr.pop()
 
-def check_l_angle(board, row, col, board_len):
-    """Check right angle for queen"""
-    c = col
-    for r in range(row, -1, -1):
-        if c < 0:
-            break
-        if board[r][c]:
-            return False
-        c -= 1
-    c = col
-    for r in range(row, board_len):
-        if c >= board_len:
-            break
-        if board[r][c]:
-            return False
-        c += 1
-    return True
+    return arr
 
-def check_all(board, r, c, n):
-    if not check_row(board, r, n):
-        return False
-    if not check_l_angle(board, r, c, n):
-        return False
-    return check_r_angle(board, r, c, n)
-
-def main():
-    """Main function"""
-    argc = len(argv)
-    if argc != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    try:
-        n = int(argv[1])
-    except Exception:
-        print("N must be a number")
-        exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
-    n_range = range(n)
-    i = 0
-    c = 0
-    r = i
-    board = [[0 for _ in n_range] for _ in n_range]
-    result = []
-    while i < n:
-        while c < n:
-            found = 0
-            while r < n:
-                if check_all(board, r, c, n):
-                    board[r][c] = 1
-                    result.append([c, r])
-                    found = 1
-                    r = 0
-                    break
-                r += 1
-            if not found and len(result):
-                last_i = result.pop()
-                c = last_i[0]
-                r = last_i[1] + 1
-                board[last_i[1]][last_i[0]] = 0
-                continue
-            c += 1
-        if len(result):
-            print(result)
-            i = result[0][1]
-            last_i = result.pop()
-            c = last_i[0]
-            r = last_i[1] + 1
-            board[last_i[1]][last_i[0]] = 0
-        else:
-            return
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except BaseException:
+        print("N must be a number")
+        exit(1)
+
+    if not isinstance(n, int):
+        print("N must be a number")
+        exit(1)
+
+    elif n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    n_q_arr = n_q([], [], [], 0, n - 1)
+    for i in n_q_arr:
+        print(i)
