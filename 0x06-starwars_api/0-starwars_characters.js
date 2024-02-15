82257048d70965f79089d026f0551a394519e10a
jerry nabango <jerrynabango9@gmail.com>
2024-02-15 22:38:04 +0300

    Prints all Characters of Star Wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,25 @@
 #!/usr/bin/node
-const req = require('request');
+// Script that prints all characters of a Star Wars movie
+const request = require('request');
 const movieId = process.argv[2];
 const options = {
-  url: `https://swapi-api.hbtn.io/api/films/${movieId}`,
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
   method: 'GET'
 };
 
-req(options, function (err, res, body) {
-  if (!err) {
-    const chars = JSON.parse(body).characters;
-    all(chars, 0);
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    all(characters, 0);
   }
 });
 
-function all(chars, index) {
-  req(chars[index], function (err, res, body) {
-    if (!err) {
+function all (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
       console.log(JSON.parse(body).name);
-      if (index + 1 < chars.length) {
-        all(chars, index + 1);
+      if (index + 1 < characters.length) {
+        all(characters, index + 1);
       }
     }
   });
