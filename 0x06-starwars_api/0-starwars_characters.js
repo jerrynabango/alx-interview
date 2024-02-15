d6b49ccc6f39d5a01d24ff49cdbecbd70931d5f1
jerry nabango <jerrynabango9@gmail.com>
2024-02-15 22:34:22 +0300

    Prints all Characters of Star Wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,16 +9,16 @@ const options = {
 req(options, function (err, res, body) {
   if (!err) {
     const chars = JSON.parse(body).characters;
-    all_characters(chars, 0);
+    all(chars, 0);
   }
 });
 
-function all_characters(chars, index) {
+function all(chars, index) {
   req(chars[index], function (err, res, body) {
     if (!err) {
       console.log(JSON.parse(body).name);
       if (index + 1 < chars.length) {
-        all_characters(chars, index + 1);
+        all(chars, index + 1);
       }
     }
   });
