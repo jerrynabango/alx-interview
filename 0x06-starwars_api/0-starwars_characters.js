2143d7c4416f32170ea99657278fd22352a890cc
jerry nabango <jerrynabango9@gmail.com>
2024-02-16 12:07:54 +0300

    API update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// Script that prints all characters of a Star Wars movie
 const request = require('request');
 const movieId = process.argv[2];
 const options = {
@@ -10,16 +9,16 @@ const options = {
 request(options, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    all(characters, 0);
+    printCharacters(characters, 0);
   }
 });
 
-function all (characters, index) {
+function printCharacters (characters, index) {
   request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
-        all(characters, index + 1);
+        printCharacters(characters, index + 1);
       }
     }
   });
