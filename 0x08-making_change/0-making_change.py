b856c732574a46473c12d49ad0f65b34a563abf6
Jerry Nabango <jerrynabango9@gmail.com>
2024-02-27 21:30:57 +0300

    fewest n.o of coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Change comes from within"""
+
+
+def makeChange(coins, total):
+    """fewest number of coins needed to meet total"""
+    if total <= 0:
+        return 0
+    few = 0
+    count = 0
+    coins.sort(key=lambda x: x, reverse=True)
+    while coins:
+        value = coins[0]
+        if few + value > total:
+            coins.pop(0)
+            continue
+        few += value
+        count += 1
+        if few == total:
+            return count
+    return -1
